0086ef10dd1c57aba8a23e9c399d117bda44a816
Borislav Pavlov <borislavgpavlov@mail>
2026-06-08 15:54:37 +0800

    Format Goal.vue with prettier

--- components/game/Goal.vue
+++ components/game/Goal.vue
@@ -511,18 +511,18 @@ function getTextColor(index: number) {
 }
 
 const targetShortcuts = [
-  '⌃0',
-  '⌃1',
-  '⌃2',
-  '⌃3',
-  '⌃4',
-  '⌃5',
-  '⌃6',
-  '⌃7',
-  '⌃8',
-  '⌃9',
-  '⌃-',
-  '⌃='
+  "⌃0",
+  "⌃1",
+  "⌃2",
+  "⌃3",
+  "⌃4",
+  "⌃5",
+  "⌃6",
+  "⌃7",
+  "⌃8",
+  "⌃9",
+  "⌃-",
+  "⌃="
 ] as const;
 
 const cellCenters: Record<number, { x: number; y: number }> = {
