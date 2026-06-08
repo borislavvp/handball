6e16f062ba4be4c1b2f83aaabd1986ae8d5a91bb
Borislav Pavlov <borislavgpavlov@mail>
2026-06-08 15:35:29 +0800

    Add useModifierState composable and ShortcutChip component

---
+++ composables/useModifierState.ts
@@ -0,0 +1,20 @@
+import { useMagicKeys } from '@vueuse/core';
+import { computed } from 'vue';
+
+export const useModifierState = () => {
+    const keys = useMagicKeys({
+        shift: 'shift',
+        ctrl: 'ctrl',
+        alt: 'alt',
+    });
+
+    const shiftHeld = computed(() => Boolean(keys.shift?.value));
+    const ctrlHeld = computed(() => Boolean(keys.ctrl?.value));
+    const altHeld = computed(() => Boolean(keys.alt?.value));
+
+    return {
+        shiftHeld,
+        ctrlHeld,
+        altHeld,
+    };
+};
